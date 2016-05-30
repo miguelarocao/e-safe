d67bb826b94070a0d35de5b78200807332b3705a
miguelarocao <mig_ao@live.com>
2016-05-29 21:58:22 -0700

    Added viterbi algorithm and added input parser

--- esafe.py
+++ esafe.py
@@ -1,12 +1,17 @@
 ## EE/CS 159 Project
 ## Miguel Aroc-Ouellette
 
-# TODO: Force final transition to end state
-# Prevent premature transition to end state
-
+# TODO: Check minimum of pages!
 
 import numpy as np
 import random as rnd
+import datetime
+import matplotlib.pyplot as plt
+
+data_dict={'user_id':0,
+           'start_time':1,
+           'end_time':2,
+           'seq':3}
 
 class eSafe:
     def __init__(self, _num_states, _num_obs, prior_trans = None, prior_obs = None):
@@ -18,7 +23,7 @@ class eSafe:
         #constant
         self.num_states =_num_states + 2                #+2 for start & end state
         self.num_obs = _num_obs
-        self.learn_rate = 1                             #learning rate
+        self.learn_rate = 0.5                           #learning rate
         self.e = 0.8                                    #probability that algorithm picks safest (most probable) event
         self.start = 0                                  #start state index
         self.end = self.num_states - 1                  #end state index
@@ -41,16 +46,16 @@ class eSafe:
     def init_matrices(self, prior_trans = None, prior_obs = None):
         """ Initialiazes the matrices randomly, or with prior information"""
 
-        if prior_trans:
+        if prior_trans is not None:
             self.trans = prior_trans
         else:
-            for row in range(self.num_states):
+            for row in range(self.num_states-1):
                 self.trans[row,1:] = np.random.dirichlet(np.ones(self.num_states-1),1)
 
-        if prior_obs:
+        if prior_obs is not None:
             self.obs = prior_obs
         else:
-            for row in range(self.num_states-1):
+            for row in range(1,self.num_states):
                 self.obs[row,:]=np.random.dirichlet(np.ones(self.num_obs),size=1)
 
         return
@@ -58,19 +63,19 @@ class eSafe:
     def train_on_seq(self):
         """ Trains on the current sequence. """
 
-        for curr_ob in self.seq:
+        for curr_ob in self.seq[:-1]: #for everything but last state
             new_state = None
             r_safe = rnd.uniform(0,1) < self.e
             if r_safe:
                 #update the most probable event
                 new_state = self.get_safe(curr_ob)
-                print "Playing it safe on observation "+str(curr_ob)
+                #print "Playing it safe on observation "+str(curr_ob)
             else:
                 #pick an event at random from the possible events
                 #   i.e. pick a random state, update trans and obs appropriately
                 new_state = rnd.choice(range(1,self.num_states-1))
-                print "Randomizing on observation "+str(curr_ob)
-            print "Picked "+str(new_state)
+                #print "Randomizing on observation "+str(curr_ob)
+            #print "Picked "+str(new_state)
             self.update_event(new_state, curr_ob)
             curr_state = new_state
 
@@ -81,24 +86,43 @@ class eSafe:
 
         return
 
-    def train(self, filename):
+    def train(self, filename, num_seq):
         """ Trains on the sequences found in the input file. Lazily read.
             Each sequence should occupy a new line and be commat separated."""
 
+        count=0
+        prob=[0]*num_seq
         with open(filename,'r') as f:
-            while True:
-                line = f.readline()
-                if line=="":
-                    break
-                self.seq = map(int,line.strip().split(','))
+            for line in f:
+                seq = line.rstrip()[1:-1].split(',')[data_dict['seq']:]
+                seq[0] = seq[0][1:]
+                seq[-1] = seq[-1][:-1]
+                self.seq = [int(x[2:-1])+1 for x in seq] #Note: +1 to handle start state!
                 self.curr_state = self.start #reset to start state
-                print "Training on: "+','.join(map(str,self.seq))
+                #print "Training on: "+','.join(map(str,self.seq))
+                prob[count]=self.get_prob_seq(self.seq)
                 self.train_on_seq()
+                count+=1
+                if count%5000==0:
+                    print "Processed "+str(count)+"..."
+                if count >= num_seq:
+                    break
 
-        print "Transition Matrix: "
-        print self.trans
-        print "Observation Matrix: "
-        print self.obs
+        #self.plot_conv(prob)
+        #print "Transition Matrix: "
+        #print self.trans
+        #print "Observation Matrix: "
+        #print self.obs
+
+        return prob
+
+    def plot_conv(self,prob):
+        plt.style.use('ggplot')
+
+        plt.plot(prob)
+        plt.ylabel('Proability')
+        plt.xlabel('Sequence count')
+        plt.show()
 
     def get_safe(self,observation):
         """ Returns the most probable state given an observation, either simply
@@ -108,29 +132,24 @@ class eSafe:
 
         safe = self.curr_state
         safe_prob = self.obs[safe,observation]*self.trans[safe, safe]
-        print "State: "+str(safe)+" has prob "+str(safe_prob)
-        for state in range(self.num_states):
+        #print "State: "+str(safe)+" has prob "+str(safe_prob)
+        for state in range(self.num_states-1): #can't go to end state!
             if state==self.curr_state:
                 continue
             prob = self.obs[state,observation]*self.trans[self.curr_state, state]
             if prob > safe_prob:
                 safe = state
                 safe_prob = prob
-            print "State: "+str(state)+" has prob "+str(prob)
+            #print "State: "+str(state)+" has prob "+str(prob)
 
-        print "Safe state: "+str(safe)+" has prob "+str(safe_prob)
+        #print "Safe state: "+str(safe)+" has prob "+str(safe_prob)
         return safe
 
     def update_event(self,state, observation):
         """ Updates the transition and observation matrices based on a new
             occurence of the input state & observation."""
 
-        #update obs based, unless going to end state
-        if state != self.end:
-            self.update_distr(self.obs[state, :], observation, self.obs_count[state,:])
-            self.obs_count[state, observation] += 1
-
-        if state!=self.curr_state:
+        if state != self.curr_state:
             #update state as well
             self.update_distr(self.trans[self.curr_state, :], state,
                                         self.state_count[self.curr_state,:])
@@ -148,7 +167,8 @@ class eSafe:
 
         #update size. Function of learning rate and relative count
         #   The more frequently is seen, the lower the smaller the update
-        update = self.learn_rate*(1 - count_vec[index]/sum(count_vec))
+        #update = self.learn_rate*(1 - count_vec[index]/sum(count_vec))
+        update = self.learn_rate
 
         if np.isnan(update): #handles division by zero
             update = self.learn_rate
@@ -160,10 +180,119 @@ class eSafe:
 
         return
 
+    def get_prob_seq(self,seq):
+        """Returns the probability of a given observation sequence.
+        Uses Viterbi Algorithm and Dynamic Programming."""
+
+        #small log numbers
+        log_zero = -1e9
+
+        #append None for start state
+        seq = list([None]+seq)
+
+        # input sequence length
+        seq_len = len(seq)
+
+        # stores P(Best sequence)
+        prob = [[[0] for i in range(self.num_states)] for j in range(seq_len)]
+
+        # Stores most likely hidden state sequence
+        state_seq = [[[''] for i in range(self.num_states)] for j in range(seq_len)]
+
+        # always start in start state
+        prob[0] = [log_zero]*self.num_states
+        prob[0][self.start] = 0
+
+        # initalize best sequence of length 1
+        state_seq[0] =[str(i) for i in range(self.num_states)]
+
+        # iterate through all observations in given sequence
+        for length in range(1, seq_len):    #skip initial state
+            for state in range(self.num_states):
+                max_state = 0
+                best_prob = log_zero #can't be 0 due to log
+
+                # iterate through all possible transitions
+                for prev in range(self.num_states):
+                    # cur_prob is the probability of transitioning to 'state'
+                    # from 'prev' state and observing the correct state.
+                    cur_prob = prob[length - 1][prev] + np.log(self.trans[prev][state]) + np.log(self.obs[state][seq[length]])
+                    if cur_prob > best_prob:
+                        max_state, best_prob = prev, cur_prob
+
+                    # update best probability
+                    prob[length][state] = best_prob
+                    # update sequence
+                    state_seq[length][state] = state_seq[length - 1][max_state] + str(state)
+
+            prob[length] = prob[length][:]   # copies by value
+            state_seq[length] = state_seq[length][:]
+
+        # get maximum sequence that brought you to end state
+        #print state_seq[-1][self.end][1:]
+
+        # return result. Note, always end on end state!
+        return np.exp(prob[-1][self.end])
+
 def main():
     # Test on sample
-    my_alg = eSafe(3,5)
-    my_alg.train("sample_data.txt")
+    path="D:\\Datasets\\ML_Datasets\\seq_data\\"
+    fname='data_2_1.txt' #max value is 3388
+    #mylearner=eSafe(4,3388)
+    #mylearner.train(path+fname,100000)
+
+    #plot while varying learning rate
+    plt.style.use('ggplot')
+    alpha=np.arange(0,1,0.1)
+    for i in range(len(alpha)):
+        mylearner=eSafe(4,3388)
+        mylearner.learn_rate=alpha[i]
+        prob = mylearner.train(path+fname,100000)
+        plt.plot(prob,label=str(alpha[i]))
+        print "Processed rate = "+str(alpha[i])
+
+    plt.legend(loc='upper right')
+    plt.ylabel('Probability')
+    plt.xlabel('Sequence count')
+    plt.show()
+
+##    myfile= open(path+fname)
+##    count = 0
+##    run_max = 0
+##    run_min = 10000
+##    for line in myfile:
+##        seq = line.rstrip()[1:-1].split(',')[data_dict['seq']:]
+##        seq[0] = seq[0][1:]
+##        seq[-1] = seq[-1][:-1]
+##        seq = [int(x[2:-1]) for x in seq]
+##        curr_max = max(seq)
+##        curr_min = min(seq)
+##        if curr_max > run_max:
+##            run_max = curr_max
+##        if curr_min < run_min:
+##            run_min = curr_min
+##        count +=1
+##        if count%10000==0:
+##            print count
+##    print count
+##    print "Max: "+str(run_max)
+##    print "Min: "+str(run_min)
+
+##    #two states healthy (0) and fever(1) -> health (1) and fever (2)
+##    #three observations dizzy (0), cold (1) and normal (2)
+##    trans = np.array([[0,0.6,0.4,0],
+##                      [0,0.6,0.3,0.1],
+##                      [0,0.3,0.6,0.1],
+##                      [0,0,0,0]])
+##    obs = np.array([[0,0,0],
+##                    [0.1,0.4,0.5],
+##                    [0.6,0.3,0.1],
+##                    [0.5,0.4,0.1]])
+##
+##    mylearner=eSafe(2,3,trans,obs)
+##
+##    #viterbi test
+##    mylearner.get_prob_seq([2,1,0])
 
 if __name__ == '__main__':
     main()
